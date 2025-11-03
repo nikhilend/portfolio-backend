5a359458e3f7f435533229dc620678b6a6dc93c5
Nikhil K <nikhilkhobragade02@gmail.com>
2025-11-03 15:08:03 +0530

    vercel changes

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ app.delete("/project/:id", (req, res)=> { deleteData(req, res)})
 
 app.post("contact", (req,res)=> insertContact(req, res))
 
-app.listen(process.env.PORT, ()=> {
-    console.log("server is listening at port : ", process.env.PORT)
-})
+// app.listen(process.env.PORT, ()=> {
+//     console.log("server is listening at port : ", process.env.PORT)
+// })
+
+module.exports = app
